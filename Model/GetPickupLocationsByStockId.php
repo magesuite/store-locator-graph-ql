d7a614595bf26ef88df7c0d184e69657860a3fb8
Zbigniew Stachyra <zbigniew.stachyra@creativestyle.pl>
2021-07-07 12:24:09 +0000

    feat: [NKD-2427] - GraphQL - pickuplocations with query - city name or postcode

--- Model/GetPickupLocationsByStockId.php
+++ Model/GetPickupLocationsByStockId.php
@@ -23,26 +23,44 @@ class GetPickupLocationsByStockId
      */
     protected $configuration;
 
+    /**
+     * @var \Magento\Framework\Filter\RemoveAccents
+     */
+    protected $removeAccents;
+
     public function __construct(
         \Magento\InventoryApi\Api\GetSourcesAssignedToStockOrderedByPriorityInterface $getSourcesAssignedToStockOrderedByPriority,
         \Magento\InventoryInStorePickupApi\Model\Mapper $mapper,
         \Magento\InventoryInStorePickup\Model\Source\GetIsPickupLocationActive $getIsPickupLocationActive,
-        \MageSuite\StoreLocatorGraphQl\Helper\Configuration $configuration
+        \MageSuite\StoreLocatorGraphQl\Helper\Configuration $configuration,
+        \Magento\Framework\Filter\RemoveAccents $removeAccents
     ) {
 
         $this->getSourcesAssignedToStockOrderedByPriority = $getSourcesAssignedToStockOrderedByPriority;
         $this->mapper = $mapper;
         $this->getIsPickupLocationActive = $getIsPickupLocationActive;
         $this->configuration = $configuration;
+        $this->removeAccents = $removeAccents;
     }
 
-    public function execute($stockId)
+    public function execute(int $stockId, string $query = ''): array
     {
+        $query = $this->removeAccents->filter($query);
+
         $sources = $this->getSourcesAssignedToStockOrderedByPriority->execute($stockId);
 
         $result = [];
         foreach ($sources as $source) {
-            if ($this->shouldSourceBeDisplayed($source)) {
+            if (!$this->shouldSourceBeDisplayed($source)) {
+                continue;
+            }
+
+            if (empty($query)) {
+                $result[] = $this->mapper->map($source);
+                continue;
+            }
+
+            if ($this->sourceMatchQuery($source, $query)) {
                 $result[] = $this->mapper->map($source);
             }
         }
@@ -58,8 +76,28 @@ class GetPickupLocationsByStockId
     {
         if ($this->configuration->getAvailabilityMode() == \MageSuite\StoreLocatorGraphQl\Model\Config\Source\AvailabilityMode::STORES_AVAILABILITY_MODE_SHOW_ENABLED_ONLY) {
             return $this->getIsPickupLocationActive->execute($source);
-        } 
-        
+        }
+
         return true;
+    }
+
+    private function sourceMatchQuery(\Magento\InventoryApi\Api\Data\SourceInterface $source, string $query): bool
+    {
+        if ($this->sourceMatchCity($source->getCity(), $query) || ($query === $source->getPostcode())) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function sourceMatchCity(string $cityString, string $query): bool
+    {
+        $cityString = $this->removeAccents->filter($cityString);
+
+        if (mb_stripos($cityString, $query) === 0) {
+            return true;
+        }
+
+        return false;
     }
 }
--- Test/Integration/Model/GetPickupLocationsByStockIdTest.php
+++ Test/Integration/Model/GetPickupLocationsByStockIdTest.php
@@ -9,7 +9,9 @@ namespace MageSuite\StoreLocatorGraphQl\Test\Integration\Model;
 
 class GetPickupLocationsByStockIdTest extends \MageSuite\StoreLocatorGraphQl\Test\Integration\AbstractTestCase
 {
-
+    /**
+     * @var \MageSuite\StoreLocatorGraphQl\Model\GetPickupLocationsByStockId
+     */
     protected $getPickupLocationsByStockId;
 
     private const STOCK_ID = 30;
@@ -55,5 +57,99 @@ class GetPickupLocationsByStockIdTest extends \MageSuite\StoreLocatorGraphQl\Tes
     {
         $sources = $this->getPickupLocationsByStockId->execute(self::STOCK_ID);
         $this->assertEquals(self::ALL_STORES_COUNT, count($sources), 'Failed to assert that all sources are returned (regardless of their pickup location status).');
+    }
+
+    /**
+     * @magentoDataFixture loadSourcesFixture
+     * @magentoDataFixture loadStocksFixture
+     * @magentoDataFixture loadStockSourceLinksFixture
+     * @magentoDataFixture loadSourceItemsFixture
+     * @magentoDataFixture loadWebsiteWithStoresFixture
+     * @magentoDataFixture loadStockWebsiteSalesChannelsFixture
+     * @magentoDataFixture loadReindexInventoryFixture
+     * @magentoConfigFixture current_store store_locator/configuration/availability_mode show_enabled_only
+     */
+    public function testItReturnsAllEnabledSourcesForEmptyQuery()
+    {
+        $sources1 = $this->getPickupLocationsByStockId->execute(self::STOCK_ID);
+        $sources2 = $this->getPickupLocationsByStockId->execute(self::STOCK_ID, '');
+        $sources3 = $this->getPickupLocationsByStockId->execute(self::STOCK_ID, '  ');
+
+        $this->assertEquals(3, count($sources1));
+        $this->assertEquals(3, count($sources2));
+        $this->assertEquals(0, count($sources3));
+    }
+
+    /**
+     * @magentoDataFixture loadSourcesFixture
+     * @magentoDataFixture loadStocksFixture
+     * @magentoDataFixture loadStockSourceLinksFixture
+     * @magentoDataFixture loadSourceItemsFixture
+     * @magentoDataFixture loadWebsiteWithStoresFixture
+     * @magentoDataFixture loadStockWebsiteSalesChannelsFixture
+     * @magentoDataFixture loadReindexInventoryFixture
+     * @magentoConfigFixture current_store store_locator/configuration/availability_mode show_enabled_only
+     */
+    public function testItReturnsSourcesForQueryWhichRelatesToCity()
+    {
+        $sources1 = $this->getPickupLocationsByStockId->execute(self::STOCK_ID, 'city');
+        $sources2 = $this->getPickupLocationsByStockId->execute(self::STOCK_ID, 'CITY');
+        $sources3 = $this->getPickupLocationsByStockId->execute(self::STOCK_ID, 'city-2');
+        $sources4 = $this->getPickupLocationsByStockId->execute(self::STOCK_ID, 'ci');
+        $sources5 = $this->getPickupLocationsByStockId->execute(self::STOCK_ID, 'ty');
+        $sources6 = $this->getPickupLocationsByStockId->execute(self::STOCK_ID, 'ty-2');
+
+        $this->assertEquals(2, count($sources1));
+        $this->assertEquals(2, count($sources2));
+        $this->assertEquals(1, count($sources3));
+        $this->assertEquals(2, count($sources4));
+        $this->assertEquals(0, count($sources5));
+        $this->assertEquals(0, count($sources6));
+    }
+
+    /**
+     * @magentoDataFixture loadSourcesFixture
+     * @magentoDataFixture loadStocksFixture
+     * @magentoDataFixture loadStockSourceLinksFixture
+     * @magentoDataFixture loadSourceItemsFixture
+     * @magentoDataFixture loadWebsiteWithStoresFixture
+     * @magentoDataFixture loadStockWebsiteSalesChannelsFixture
+     * @magentoDataFixture loadReindexInventoryFixture
+     * @magentoConfigFixture current_store store_locator/configuration/availability_mode show_enabled_only
+     */
+    public function testItReturnsSourcesForQueryWhichRelatesToPostcode()
+    {
+        $sources1 = $this->getPickupLocationsByStockId->execute(self::STOCK_ID, 'postcode-3');
+        $sources2 = $this->getPickupLocationsByStockId->execute(self::STOCK_ID, 'POSTCODE-3');
+        $sources3 = $this->getPickupLocationsByStockId->execute(self::STOCK_ID, 'post');
+        $sources4 = $this->getPickupLocationsByStockId->execute(self::STOCK_ID, 'code-3');
+
+        $this->assertEquals(1, count($sources1));
+        $this->assertEquals(0, count($sources2));
+        $this->assertEquals(0, count($sources3));
+        $this->assertEquals(0, count($sources4));
+    }
+
+    /**
+     * @magentoDataFixture loadSourcesFixture
+     * @magentoDataFixture loadStocksFixture
+     * @magentoDataFixture loadStockSourceLinksFixture
+     * @magentoDataFixture loadSourceItemsFixture
+     * @magentoDataFixture loadWebsiteWithStoresFixture
+     * @magentoDataFixture loadStockWebsiteSalesChannelsFixture
+     * @magentoDataFixture loadReindexInventoryFixture
+     * @magentoConfigFixture current_store store_locator/configuration/availability_mode show_enabled_only
+     */
+    public function testItReturnsSourcesForQueryWithAccentsWhichRelatesToCity()
+    {
+        $sources1 = $this->getPickupLocationsByStockId->execute(self::STOCK_ID, 'Königsbrück');
+        $sources2 = $this->getPickupLocationsByStockId->execute(self::STOCK_ID, 'Konigsbruck');
+        $sources3 = $this->getPickupLocationsByStockId->execute(self::STOCK_ID, 'König');
+        $sources4 = $this->getPickupLocationsByStockId->execute(self::STOCK_ID, 'konig');
+
+        $this->assertEquals(1, count($sources1));
+        $this->assertEquals(1, count($sources2));
+        $this->assertEquals(1, count($sources3));
+        $this->assertEquals(1, count($sources4));
     }
 }
